30b546662c45d97da32b6e0d8cc1962393c1648c
Iskander <iskander73rus@mail.ru>
2021-02-10 13:54:09 +0400

    New App Ready

---
+++ src/main/java/management/mail/MailApplication.java
@@ -0,0 +1,20 @@
+package management.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MailApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MailApplication.class);
+	}
+}
+
